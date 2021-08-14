7de5feaa083aa5b1f42b5d54adc9f940d73baaf7
Ivy Siewert <jivysiewert@gmail.com>
2021-08-13 21:42:47 -0500

    Fetch Post Comment

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -34,7 +34,7 @@ class CommentForm extends Component {
         }
         handleSubmit(values) {
             this.toggleModal();
-            this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
+            this.props.postComment(this.props.campsiteId, values.rating, values.author, values.text);
         }
 
     render() {
@@ -102,7 +102,7 @@ class CommentForm extends Component {
         );
     }
 
-    function RenderComments({comments, addComment, campsiteId}) {
+    function RenderComments({comments, postComment, campsiteId}) {
         if (comments) {
             return (
                 <div className="col-md-5 m-1">
@@ -116,7 +116,7 @@ class CommentForm extends Component {
                             </div>
                         );
                     })}
-                    <CommentForm campsiteId={campsiteId} addComment={addComment} />
+                    <CommentForm campsiteId={campsiteId} postComment={postComment} />
                 </div>
             );
         }
@@ -161,7 +161,7 @@ class CommentForm extends Component {
                         <RenderCampsite campsite={props.campsite} />
                         <RenderComments 
                         comments={props.comments}
-                        addComment={props.addComment}
+                        postComment={props.postComment}
                         campsiteId={props.campsite.id}
                         />
                     </div>
